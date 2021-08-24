6e5f42a5e4bbb4cf4ae5cca7fa889822782fcd9b
PrashanthThugaiah <prashanth.thugaiah@gmail.com>
2021-08-24 16:31:11 +0530

    Added new component - Videoplayer

---
+++ src/components/videoplayer/index.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent, h } from 'preact';
+
+const Videoplayer: FunctionalComponent = () => {
+    return (
+        <div >
+            <h1>Videoplayer</h1>
+        </div>
+    );
+};
+
+export default Videoplayer;
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,11 +1,12 @@
 import { FunctionalComponent, h } from 'preact';
 import style from './style.css';
+import Videoplayer from '../../components/videoplayer'
 
 const Home: FunctionalComponent = () => {
     return (
         <div class={style.home}>
             <h1>Home</h1>
-            <p>This is the Home component.</p>
+            <Videoplayer/>
         </div>
     );
 };
