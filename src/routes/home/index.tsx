c62b797931a27f2ec098ac5944b6bccefad85519
PrashanthThugaiah <prashanth.thugaiah@gmail.com>
2021-08-25 18:08:59 +0530

    Integrated video.js - Videoplayer

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,14 +1,25 @@
-import { FunctionalComponent, h } from 'preact';
-import style from './style.css';
-import Videoplayer from '../../components/videoplayer'
+import { FunctionalComponent, h } from "preact";
+import style from "./style.css";
+import Videoplayer from "../../components/videoplayer";
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [
+    {
+      src: "http://vjs.zencdn.net/v/oceans.mp4",
+      type: "video/mp4",
+    },
+  ],
+};
 
 const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
-            <h1>Home</h1>
-            <Videoplayer/>
-        </div>
-    );
+  return (
+    <div class={style.home}>
+      <h1>Home</h1>
+      <Videoplayer {...videoJsOptions} />
+    </div>
+  );
 };
 
 export default Home;
